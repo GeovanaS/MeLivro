da4a509c5f4dea59c15aea7fda3073757f3e39b8
GeovanaS <geovanasilvadasilveira1@gmail.com>
2018-10-20 23:23:11 +0000

    modificando posição do nome do autor

--- js/Controler.js
+++ js/Controler.js
@@ -1,11 +1,11 @@
 var json = 
 {
-	0:{categoria:'livro',link: '#', imagem: 'img/livrodoscodigos.jpg', titulo:'Livro dos Códigos', descricao:'R$30.00',autor:'Simon Singh'},
-	1:{categoria:'livro',link: '#', imagem: 'img/oandardobebado.jpg', titulo:'O Andar do Bêbado', descricao:'R$37.00',autor:'Leonard Mlodinow'},
-	2:{categoria:'livro',link: '#', imagem: 'img/laranjamecanica.jpg', titulo:'Laranja Mecânica', descricao:'R$25.00',autor:'Anthony Burgess'},
-	3:{categoria:'livro',link: '#', imagem: 'img/alice.jpg', titulo:'Alice no País das Maravilhas', descricao:'R$20.00',autor:'Lewis Carroll'},
-	4:{categoria:'livro',link: '#', imagem: 'img/revolucaodosbichos.jpg', titulo:'A revolução dos bichos', descricao:'R$23.90',autor:'George Orwell'},
-	5:{categoria:'livro',link: '#', imagem: 'img/engenhariasoftware.jpg', titulo:'Engenharia de Software', descricao:'R$90.00',autor:'Ian Sommerville'},
+	0:{categoria:'livro',link: '#', imagem: 'img/livrodoscodigos.jpg', titulo:'Livro dos Códigos',autor:'Simon Singh',preco:'R$30.00'},
+	1:{categoria:'livro',link: '#', imagem: 'img/oandardobebado.jpg', titulo:'O Andar do Bêbado',autor:'Leonard Mlodinow',preco:'R$37.00'},
+	2:{categoria:'livro',link: '#', imagem: 'img/laranjamecanica.jpg', titulo:'Laranja Mecânica',autor:'Anthony Burgess',preco:'R$25.00'},
+	3:{categoria:'livro',link: '#', imagem: 'img/alice.jpg', titulo:'Alice no País das Maravilhas',autor:'Lewis Carroll',preco:'R$20.00'},
+	4:{categoria:'livro',link: '#', imagem: 'img/revolucaodosbichos.jpg', titulo:'A revolução dos bichos',autor:'George Orwell',preco:'R$23.90'},
+	5:{categoria:'livro',link: '#', imagem: 'img/engenhariasoftware.jpg', titulo:'Engenharia de Software',autor:'Ian Sommerville',preco:'R$90.00'},
 };
 
 
@@ -58,8 +58,8 @@ function listaItens(objeto){
    	 var li = '<li class="ui-li-has-thumb ui-first-child"><a href="' +objeto[ind]['link']+'" class="ui-btn">'+
 			  '<img src="'+objeto[ind]['imagem']+'" class="ui-li-thumb">'+
 			  '<h2>'+objeto[ind]['titulo']+'</h2>'+
-			  '<p>'+objeto[ind]['descricao']+'</p>'+
-			  '<p class="ui-li-aside">'+objeto[ind]['autor']+'</p>'+
+			  '<p>'+objeto[ind]['autor']+'</p>'+
+			  '<p class="ui-li-aside">'+objeto[ind]['preco']+'</p>'+
 			  '</a></li>';
 			  $('#saidaTxt').append(li);
    }
